d00ebfa92e84dd3e7a8d192e2c84c0ef677ff81e
Michael VanderHelm <mvanderhelm@redapt.com>
2022-03-11 10:23:37 -0800

    end of 4

--- APM-Start/src/app/app.component.ts
+++ APM-Start/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'pm-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'mv-root',
+  template: `<div>
+    <h1>{{ pageTitle }}</h1>
+    <div>first component</div>
+  </div>`,
 })
 export class AppComponent {
-  title = 'Angular: Getting Started';
+  pageTitle: string = 'MV mgmt';
 }
